34bbe9f5ba8a2a4f46bfbdcbaaf71416a62c2b43
Matthew Struensee <mstruensee@gmail.com>
2021-08-02 13:03:44 -0500

    single-spa-inspector

--- src/root.component.js
+++ src/root.component.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { BrowserRouter, Link } from "react-router-dom";
 import "./root.component.css";
-export default function Root() {
-  return (
-    <BrowserRouter>
-      <nav className="nav">
-        <Link to="/page1" className="link">
-          Page 1
-        </Link>
-        <Link to="/page2" className="link">
-          Page 2
-        </Link>
-      </nav>
-    </BrowserRouter>
-  );
-}
+
+export const RootComponent = () => (
+  <BrowserRouter>
+    <nav className="nav">
+      <Link to="/page1" className="link">
+        Page 1
+      </Link>
+      <Link to="/page2" className="link">
+        Page 2
+      </Link>
+    </nav>
+  </BrowserRouter>
+);
--- src/root.component.test.js
+++ src/root.component.test.js
@@ -1,9 +1,9 @@
-import { render } from "@testing-library/react";
-import Root from "./root.component";
+import {render} from "@testing-library/react";
+import {RootComponent} from "./root.component";
 
 describe("Root component", () => {
-  it("should be in the document", () => {
-    const { getByText } = render(<Root name="Testapp" />);
-    expect(getByText(/Page 1/i)).toBeInTheDocument();
-  });
+    it("should be in the document", () => {
+        const {getByText} = render(<RootComponent/>);
+        expect(getByText(/Page 1/i)).toBeInTheDocument();
+    });
 });
